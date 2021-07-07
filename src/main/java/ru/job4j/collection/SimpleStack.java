7181d4c4a307947a2633c0e31a07deb52f3bb333
Barmaley <e.g.zakharov@gmail.com>
2021-07-07 20:26:40 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -4,9 +4,7 @@ public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
 
     public T pop() {
-        T firstElement = linked.iterator().next();
-        linked.deleteFirst();
-        return firstElement;
+        return linked.deleteFirst();
     }
 
     public void push(T value) {
