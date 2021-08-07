27911898bd4a02deef6d38511d067c2245f5ba44
Barmaley <e.g.zakharov@gmail.com>
2021-08-07 17:27:20 +0300

    1. Создать модель User [#999 #178003]

--- src/main/java/ru/job4j/user/User.java
+++ src/main/java/ru/job4j/user/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.user;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
 
@@ -16,5 +16,59 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User userFirst = new User();
+        userFirst.name = "Vasya";
+        userFirst.children = 1;
+        userFirst.birthday = new GregorianCalendar(2014, Calendar.APRIL, 21);
+        User userSecond = new User();
+        userSecond.name = "Vasya";
+        userSecond.children = 1;
+        userSecond.birthday = new GregorianCalendar(2014, Calendar.APRIL, 21);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(userFirst, new Object());
+        map.put(userSecond, new Object());
+
+        System.out.println("Print map: " + map);
+
+        System.out.println("Hashcode userFirst key: " + userFirst.hashCode());
+        System.out.println("Hashcode userFirst value: " + map.get(userFirst).hashCode());
+        System.out.println("Hashcode userSecond key: " + userSecond.hashCode());
+        System.out.println("Hashcode userSecond value: " + map.get(userSecond).hashCode());
+
+        System.out.println("hashCode equals: " + (map.get(userFirst).hashCode() == map.get(userSecond).hashCode()));
+        System.out.println("equals: " + map.get(userFirst).equals(map.get(userSecond)));
+
+        System.out.println("userFirst: " + map.get(userFirst));
+        System.out.println("userSecond: " + map.get(userSecond));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && name.equals(user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
     }
 }
