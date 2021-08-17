8d751309de42fe8918aad6ef9c167fc43303d080
Barmaley <e.g.zakharov@gmail.com>
2021-08-17 13:03:06 +0300

    4.0. File [#252491 #190495]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -5,6 +5,9 @@ import java.io.File;
 public class Dir {
 
     public static void main(String[] args) {
+        final int GB_SIZE = 1073741824;
+        final int KB_SIZE = 1024;
+
         File file = new File("C:\\Users\\Barmaley\\IdeaProjects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
@@ -12,9 +15,9 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.printf("Total disk size: %s Gb%n", file.getTotalSpace() / 1073741824);
+        System.out.printf("Total disk size: %s Gb%n", file.getTotalSpace() / GB_SIZE);
         for (File subfile : file.listFiles()) {
-            System.out.printf("%s: %s Kb%n", subfile.getName(), subfile.length() / 1024);
+            System.out.printf("%s: %s Kb%n", subfile.getName(), subfile.length() / KB_SIZE);
         }
     }
 }
