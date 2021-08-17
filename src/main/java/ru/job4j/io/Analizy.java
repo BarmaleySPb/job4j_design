c1387e5560c5223a373528922818b1ae3d5f7ce4
Barmaley <e.g.zakharov@gmail.com>
2021-08-17 13:50:40 +0300

    2. Анализ доступности сервера. [#859 #190459]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,34 +1,29 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Analizy {
 
     public static void unavailable(String source, String target) {
-        Map<String, String> map = new LinkedHashMap<>();
+        AtomicBoolean active = new AtomicBoolean(false);
 
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+
             in.lines().forEach(line -> {
                 String[] status = line.split(" ");
-                map.put(status[1], Integer.parseInt(status[0]) > 300 ? "start" : "finish");
+                int statusCode = Integer.parseInt(status[0]);
+                boolean req = active.get();
+                if (statusCode > 300 && !req) {
+                    active.set(true);
+                    out.printf("%s%s", status[1], ";");
+                }
+                if (statusCode <= 300 && req) {
+                    active.set(false);
+                    out.printf("%s%s%n", status[1], ";");
+                }
             });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        boolean active = false;
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (String key : map.keySet()) {
-                if (map.get(key).contains("start") && !active) {
-                    active = true;
-                    out.printf("%s%s", key, ";");
-                }
-                if (map.get(key).contains("finish") && active) {
-                    active = false;
-                    out.printf("%s%s%n", key, ";");
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
