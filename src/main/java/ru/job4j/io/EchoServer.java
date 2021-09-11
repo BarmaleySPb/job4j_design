e1b3d828217dee60dc0b121fe1f3bf1cea33a6e2
Barmaley <e.g.zakharov@gmail.com>
2021-09-11 12:39:34 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,24 +3,37 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
 
 public class EchoServer {
 
     public static void main(String[] args) throws IOException {
+
+        Map<String, String> commands = Map.of(
+                "/?msg=Exit", "Server down.",
+                "/?msg=Hello", "Hello.",
+                "/?msg=", "What?"
+        );
+
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream()))) {
+                             new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("/?msg=Bye ")) {
-                            System.out.println("Server down.");
-                            server.close();
-                            break;
+                        String[] command = str.split(" ");
+                        if (str.contains("/?msg=") && commands.get(command[1]) != null) {
+                            out.write(commands.get(command[1]).getBytes());
+                            if (command[1].contains("Exit")) {
+                                server.close();
+                            }
+                        } else {
+                            out.write(("What?").getBytes());
                         }
+                        break;
                     }
                     out.flush();
                 }
