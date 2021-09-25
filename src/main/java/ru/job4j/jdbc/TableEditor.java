cbf7ad05e78a6c1bffe2d2eb3763f43f505d30c0
BarmaleySPb <e.g.zakharov@gmail.com>
2021-09-25 16:41:32 +0300

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,9 +1,6 @@
 package ru.job4j.jdbc;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
 
@@ -13,30 +10,46 @@ public class TableEditor implements AutoCloseable {
 
     private Properties properties;
 
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
         this.properties = properties;
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    private void action(String string) {
+        try (var statement = connection.createStatement()) {
+            statement.execute(string);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void createTable(String tableName) {
+        action(String.format("create table if not exists %s();", tableName));
     }
 
     public void dropTable(String tableName) {
+        action(String.format("drop table if exists %s cascade;", tableName));
     }
 
     public void addColumn(String tableName, String columnName, String type) {
+        action(String.format("alter table %s add column %s %s;", tableName, columnName, type));
     }
 
     public void dropColumn(String tableName, String columnName) {
+        action(String.format("alter table %s drop column %s cascade", tableName, columnName));
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
+        action(String.format("alter table %s rename column %s to %s;", tableName, columnName, newColumnName));
     }
-
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
         var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
