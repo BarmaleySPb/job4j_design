d975795d88157d391a0516b900d6cf1e9cf1f9a3
Barmaley <e.g.zakharov@gmail.com>
2021-11-27 20:51:33 +0300

    editing tests

--- src/test/java/ru/job4j/design/srp/ReportEngineTest.java
+++ src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -108,11 +108,9 @@ public class ReportEngineTest {
     @Test
     public void whenGenerateXmlReport() {
         MemStore store = new MemStore();
-        Calendar hired = new GregorianCalendar(2015, Calendar.FEBRUARY, 2);
-        Calendar fired = new GregorianCalendar(2020, Calendar.JANUARY, 10);
-        Employee workerFirst = new Employee("Ivan", fired, hired, 100);
-        Employee workerSecond = new Employee("Petr", fired, hired, 190);
-        Employee workerThird = new Employee("Olga", fired, hired, 150);
+        Employee workerFirst = new Employee("Ivan", null, null, 100);
+        Employee workerSecond = new Employee("Petr", null, null, 190);
+        Employee workerThird = new Employee("Olga", null, null, 150);
         store.add(workerFirst);
         store.add(workerSecond);
         store.add(workerThird);
@@ -121,8 +119,6 @@ public class ReportEngineTest {
                 .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
                 .append("<employees>\n")
                 .append("    <employees>\n")
-                .append("        <fired>2015-02-02T00:00:00+03:00</fired>\n")
-                .append("        <hired>2020-01-10T00:00:00+03:00</hired>\n")
                 .append("        <name>Petr</name>\n")
                 .append("        <salary>190.0</salary>\n")
                 .append("    </employees>\n")
@@ -133,14 +129,18 @@ public class ReportEngineTest {
     @Test
     public void whenGenerateJsonReport() {
         MemStore store = new MemStore();
-        Employee workerFirst = new Employee("Ivan", null, null, 100);
-        Employee workerSecond = new Employee("Petr", null, null, 190);
-        Employee workerThird = new Employee("Olga", null, null, 150);
+        Calendar hired = new GregorianCalendar(2015, Calendar.FEBRUARY, 2);
+        Calendar fired = new GregorianCalendar(2020, Calendar.JANUARY, 10);
+        Employee workerFirst = new Employee("Ivan", fired, hired, 100);
+        Employee workerSecond = new Employee("Petr", fired, hired, 190);
+        Employee workerThird = new Employee("Olga", fired, hired, 150);
         store.add(workerFirst);
         store.add(workerSecond);
         store.add(workerThird);
         Report engine = new ReportJson(store);
-        String expect = "{\"name\":\"Ivan\",\"salary\":100.0}";
+        String expect = "{\"name\":\"Ivan\",\"hired\":{\"year\":2020,\"month\":0,\"dayOfMonth\":10,"
+                + "\"hourOfDay\":0,\"minute\":0,\"second\":0},\"fired\":{\"year\":2015,\"month\":1,"
+                + "\"dayOfMonth\":2,\"hourOfDay\":0,\"minute\":0,\"second\":0},\"salary\":100.0}";
         Assert.assertEquals(engine.generate(m -> m.getSalary() < 150), expect);
     }
 
